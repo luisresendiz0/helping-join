991cc8c5f80229c2eaf5195354016ebec5cb2782
Omar Ramirez <omragayt@gmail.com>
2023-03-31 22:11:43 -0600

    Se agregó un mapa estático con coordenadas aleatorias y se agregó texto donde van a apparecer los detalles del evento

--- src/routes/screens/EventScreen.tsx
+++ src/routes/screens/EventScreen.tsx
@@ -1,12 +1,37 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
+import mapboxgl from "mapbox-gl";
+import React, { useRef, useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcnJhbWciLCJhIjoiY2xmdDA1ZG5pMDYwczNmcXl4bWMwNDg1ZCJ9.Q4kaTXVoH5VIfR1mgiRBRg';
 
 const EventScreen = () => {
   const query = useParams();
 
+  const mapContainer = useRef<any>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(-99.14069366766122);
+  const [lat, setLat] = useState(19.438492197400965);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+  
   return (
     <Box>
       <Heading mb={8}>{`Detalle de evento ${query.id}`}</Heading>
+      <Text>Descripción</Text>
+      <Text>Fecha inicio</Text>
+      <Text>Fecha fin</Text>
+      <Text>Dirección</Text>
+      <div ref={mapContainer} className="map-container" style={{height:"400px"}}/>
     </Box>
   );
 };
