ee2741e99cff1a7082198ff8863b27f107be7794
Omar Ramirez <omragayt@gmail.com>
2023-04-04 01:21:43 -0600

    Se agregaron elementos para geocodificacion

--- src/routes/screens/EventScreen.tsx
+++ src/routes/screens/EventScreen.tsx
@@ -1,7 +1,8 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import mapboxgl from "mapbox-gl";
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import { useParams } from "react-router-dom";
+import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcnJhbWciLCJhIjoiY2xmdDA1ZG5pMDYwczNmcXl4bWMwNDg1ZCJ9.Q4kaTXVoH5VIfR1mgiRBRg';
 
@@ -12,17 +13,82 @@ const EventScreen = () => {
   const map = useRef<mapboxgl.Map | null>(null);
   const [lng, setLng] = useState(-99.14069366766122);
   const [lat, setLat] = useState(19.438492197400965);
-  const [zoom, setZoom] = useState(9);
+  const [zoom, setZoom] = useState(14);
+
+  const fetchData = useCallback(() => {
+    const geocodingClient = mbxGeocoding({
+      accessToken: mapboxgl.accessToken,
+    });
+
+    // geocoding with countries
+    return geocodingClient
+      .forwardGeocode({
+        query: 'Av. Juárez S/N, Centro Histórico de la Cdad. de México, Centro, Cuauhtémoc, 06050 Ciudad de México, CDMX',
+        countries: ['mx'],
+        limit: 2,
+        language: ['es']
+      })
+      .send()
+      .then((response) => {
+        const match = response.body;
+        const coordinates = match.features[0].geometry.coordinates;
+        const placeName = match.features[0].place_name;
+        const center = match.features[0].center;
+
+        return {
+          type: 'Feature',
+          center: center,
+          geometry: {
+            type: 'Point',
+            coordinates: coordinates,
+          },
+          properties: {
+            description: placeName,
+          },
+        };
+      });  
+  },[]);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
+
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
       zoom: zoom
     });
-  });
+
+  }, []);
+
+  useEffect(() => {
+    if (!map.current) return; // Waits for the map to initialise
+
+    const results = fetchData();
+
+    results.then((marker) => {
+      // create a HTML element for each feature
+      var el = document.createElement('div');
+      el.className = 'marker';
+      let coord: [number,number] = [marker.geometry.coordinates.at(0)! , marker.geometry.coordinates.at(1)!]; 
+      let cen: [number,number] = [marker.center.at(0)!,marker.center.at(1)!];
+
+      // make a marker for each feature and add it to the map
+      new mapboxgl.Marker(el)
+        .setLngLat(coord)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<p>' + marker.properties.description + '</p>')
+        )
+        .addTo(map.current!);
+
+      map.current!.on('load', async () => {
+        map.current!.flyTo({
+          center: cen,
+        });
+      });
+    });
+  }, [fetchData]);
   
   return (
     <Box>
