a6411aba3d40ada086c9a2ffc7b6af084604a22a
luis.resendiz <luis.resendiz@digitex.ltd>
2023-05-26 20:16:31 -0600

    try to minify chunck size 3

--- vite.config.ts
+++ vite.config.ts
@@ -43,19 +43,19 @@ export default defineConfig({
     }),
   ],
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 2000,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes("node_modules")) {
+    //         return id
+    //           .toString()
+    //           .split("node_modules/")[1]
+    //           .split("/")[0]
+    //           .toString();
+    //       }
+    //     },
+    //   },
+    // },
   },
 });
