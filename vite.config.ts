e786e39afcd6858b9df707c6c9801bd41eafdf3f
luis.resendiz <luis.resendiz@digitex.ltd>
2023-05-26 19:35:38 -0600

    for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,35 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      includeAssets: [
+        "vite.svg",
+        "android-launchericon-192-192.png",
+        "android-launchericon-512x512.png",
+      ],
+      manifest: {
+        name: "Helping Join",
+        short_name: "Helping Join",
+        description:
+          "Vinculación de organizaciones de beneficencia e independientes con voluntarios",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-launchericon-192-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-launchericon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "android-launchericon-512x512.png",
+            sizes: "512x512",
+            type: "any maskable",
+          },
+        ],
+      },
     }),
   ],
 });
