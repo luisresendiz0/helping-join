ef5aab04ff28fb04dd525c47adb246f22233127f
Luis Resendiz <80994311+resendiz4800@users.noreply.github.com>
2023-06-23 00:34:14 -0600

    Update RecommendationsScreen.tsx

--- src/routes/screens/RecommendationsScreen.tsx
+++ src/routes/screens/RecommendationsScreen.tsx
@@ -71,7 +71,7 @@ const RecommendationsScreen = () => {
       <Heading mb={8}>Recomendaciones</Heading>
       <SimpleGrid columns={[1, 4]} spacing={8} w="full">
         {recomendations.map((recomendation) => {
-          let proba = (recomendation.probabilidad + 0.5) * 100;
+          let proba = (recomendation.probabilidad) * 1000;
           let strproba = `${proba}`.substring(0, 5) + "%";
           return (
             <Box
